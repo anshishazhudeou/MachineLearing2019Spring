b944eb22f0018012ea0b8b77b832e5eb8e82cb9a
Ricky Zhou <l62zhou@edu.uwaterloo.ca>
2019-05-25 16:41:21 -0400

    before modify

---
+++ A1/q1/knn.py
@@ -0,0 +1,15 @@
+import numpy as np
+from sklearn.neighbors import NearestNeighbors
+
+# 10 data block
+D = np.empty(shape=[10,1])
+
+# 10 label block
+L = np.empty(shape=[10,1])
+
+# initialise blocks
+
+for i in range(10):
+
+
+print("garbage")
